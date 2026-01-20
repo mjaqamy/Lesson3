854823152659a737aa6a8f5a67104f2e0998fbde
alexander.khavronin <alexander.khavronin@betera.by>
2026-01-20 14:13:07 +0300

    init: create project

---
+++ task2.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const startNum: number = 1;
+const endNum: number = 20;
+
+const result: string = "0";
+
+//Написать программу, которая выводит сумму всех чисел от 1 до 20
+
+console.log(result);
