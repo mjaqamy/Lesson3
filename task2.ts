a77eb5fabf4f9577f64536ea1672c36062a017fd
mjaqamy <mjaqamy@gmail.com>
2026-01-21 21:56:49 +0300

    feat: resolve task2

--- task2.ts
+++ task2.ts
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const startNum: number = 1;
 const endNum: number = 20;
 
-const result: string = "0";
-
+let result: string = "0";
 //Написать программу, которая выводит сумму всех чисел от 1 до 20
-
+for (let i = startNum; i <= endNum; i++) {
+    result = String(+result + i);
+}
 console.log(result);
