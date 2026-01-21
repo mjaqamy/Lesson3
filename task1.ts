c397b5da6dbc98e4b8a6114fe748e58fed101238
mjaqamy <mjaqamy@gmail.com>
2026-01-21 21:33:12 +0300

    refactor: add consts

--- task1.ts
+++ task1.ts
@@ -1,5 +1,8 @@
-const age: number = 23;
-
+const age = 23;
+const ageInDays = age * 365;
+const ageInHours = ageInDays * 24;
+const ageInMin = ageInHours * 60;
+const ageInSec = ageInMin * 3600;
 //добавить минуты
 
-console.log(`Мне ${age * 365} дней ${age * 365 * 24} часов и ${age * 365 * 24 * 3600} секунд`);
+console.log(`Мне ${ageInDays} дней ${ageInHours} часов ${ageInMin} и ${ageInSec} секунд`);
